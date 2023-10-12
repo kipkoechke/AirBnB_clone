5f70b0f32a0abdb245c9e566be4572d24d10a1eb
kipkoechke <chepkwonyke1@gmail.com>
2023-10-12 20:25:08 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel with id, created_at, and updated_at attributes.
+        If kwargs is not empty, it populates the instance attributes from the provided dictionary.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
@@ -17,7 +18,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def save(self):
         """
