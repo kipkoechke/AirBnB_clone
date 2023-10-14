327ad5155a887ae1509ec6b177d9c82301df8e07
kipkoechke <chepkwonyke1@gmail.com>
2023-10-14 23:43:13 +0300

    All instances by class name

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 def parse(arg):
     """
@@ -43,6 +49,15 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
